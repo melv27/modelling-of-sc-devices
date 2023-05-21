ebf2097178ecf58026e113e03b815344cb33cfd2
melvho <hodzic.melvin@gmx.at>
2023-05-21 17:07:14 +0200

    added new plots for 0 vs 0.5 V

--- main_IV.m
+++ main_IV.m
@@ -25,7 +25,7 @@ device.material = silicon_material_properties(T0);
 % physical constants and parameters
 secs1d_physical_constants;
 
-device.doping.NA = 5E17; % [m^3]
+device.doping.NA = 1E16; % [m^3]
 device.doping.ND = 1E17; % [m^3]
 
 % set device geometry
@@ -65,7 +65,7 @@ number_voltages = floor((voltage_end-voltage_start)/voltage_step)+1;
 
 % this array stores all voltages for which the currents will be calculated
 %voltage_ramp = linspace(voltage_start,voltage_end, number_voltages);
-voltage_ramp = [0, 0.7];
+voltage_ramp = [0, 0.5];
 
 find_zero_voltage = find(voltage_ramp == 0);
 if (find_zero_voltage > 0)
@@ -230,4 +230,4 @@ set(gca,'yscale','log');
 %saveas(figure(8),['plots','/current_density_log_NA',num2str(device.doping.NA),'exc1_inf.png']);
 hold off;
 
-%close all;
+close all;
